12d27cddd1163ca444dc3bb323dcc536bdf7c38c
YeakubDurjoy001 <your.email@example.com>
2024-02-03 10:37:44 +0600

    done withthe square function

---
+++ function.js
@@ -0,0 +1,8 @@
+function square(number) {
+  console.log("value of the given number", number);
+  const borgo = number * number;
+  console.log("square of the given number", borgo);
+}
+square(5);
+console.log("----------------");
+square(405);
